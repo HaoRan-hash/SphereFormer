a1d8cce3e8744901370de63363d82fc92579f647
haoran <2627963568@qq.com>
2023-12-15 21:40:25 +0800

    add correct pth

---
+++ correct_pth.py
@@ -0,0 +1,191 @@
+import os
+import time
+import random
+import numpy as np
+import logging
+import argparse
+import shutil
+import zlib
+import glob
+
+import torch
+import torch.backends.cudnn as cudnn
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.nn.parallel
+import torch.optim
+import torch.utils.data
+import torch.multiprocessing as mp
+import torch.distributed as dist
+import torch.optim.lr_scheduler as lr_scheduler
+from tensorboardX import SummaryWriter
+
+from util import config, transform
+from util.common_util import AverageMeter, intersectionAndUnionGPU, find_free_port
+from util.data_util import collate_fn_limit, collation_fn_voxelmean, collation_fn_voxelmean_tta
+from util.logger import get_logger
+from util.lr import MultiStepWithWarmup, PolyLR, PolyLRwithWarmup, Constant
+
+from util.nuscenes import nuScenes
+from util.semantic_kitti import SemanticKITTI
+from util.waymo import Waymo
+
+from functools import partial
+import pickle
+import yaml
+from torch_scatter import scatter_mean
+import spconv.pytorch as spconv
+
+def get_parser():
+    parser = argparse.ArgumentParser(description='PyTorch Point Cloud Semantic Segmentation')
+    parser.add_argument('--config', type=str, default='config/s3dis/s3dis_stratified_transformer.yaml', help='config file')
+    parser.add_argument('opts', help='see config/s3dis/s3dis_stratified_transformer.yaml for all options', default=None, nargs=argparse.REMAINDER)
+    args = parser.parse_args()
+    assert args.config is not None
+    cfg = config.load_cfg_from_cfg_file(args.config)
+    if args.opts is not None:
+        cfg = config.merge_cfg_from_list(cfg, args.opts)
+    return cfg
+
+
+def worker_init_fn(worker_id):
+    random.seed(args.manual_seed + worker_id)
+
+
+def main_process():
+    return not args.multiprocessing_distributed or (args.multiprocessing_distributed and args.rank % args.ngpus_per_node == 0)
+
+
+def main():
+    args = get_parser()
+
+    os.environ["CUDA_VISIBLE_DEVICES"] = ','.join(str(x) for x in args.train_gpu)
+    if not os.path.exists(args.save_path):
+        os.makedirs(args.save_path)
+    # import torch.backends.mkldnn
+    # ackends.mkldnn.enabled = False
+    # os.environ["LRU_CACHE_CAPACITY"] = "1"
+    # cudnn.deterministic = True
+    if args.manual_seed is not None:
+        random.seed(args.manual_seed)
+        np.random.seed(args.manual_seed)
+        torch.manual_seed(args.manual_seed)
+        torch.cuda.manual_seed(args.manual_seed)
+        torch.cuda.manual_seed_all(args.manual_seed)
+        cudnn.benchmark = False
+        cudnn.deterministic = True
+    if args.dist_url == "env://" and args.world_size == -1:
+        args.world_size = int(os.environ["WORLD_SIZE"])
+    args.distributed = args.world_size > 1 or args.multiprocessing_distributed
+    args.ngpus_per_node = len(args.train_gpu)
+    if len(args.train_gpu) == 1:
+        args.sync_bn = False
+        args.distributed = False
+        args.multiprocessing_distributed = False
+
+    if args.multiprocessing_distributed:
+        port = find_free_port()
+        args.dist_url = f"tcp://127.0.0.1:{port}"
+        args.world_size = args.ngpus_per_node * args.world_size
+        mp.spawn(main_worker, nprocs=args.ngpus_per_node, args=(args.ngpus_per_node, args))
+    else:
+        main_worker(args.train_gpu, args.ngpus_per_node, args)
+        
+        
+def main_worker(gpu, ngpus_per_node, argss):
+    global args, best_iou
+    args, best_iou = argss, 0
+    if args.distributed:
+        if args.dist_url == "env://" and args.rank == -1:
+            args.rank = int(os.environ["RANK"])
+        if args.multiprocessing_distributed:
+            args.rank = args.rank * ngpus_per_node + gpu
+        dist.init_process_group(backend=args.dist_backend, init_method=args.dist_url, world_size=args.world_size, rank=args.rank)
+    
+    # get model
+    if args.arch == 'unet_spherical_transformer':
+        from model.unet_spherical_transformer import Semantic as Model
+        
+        args.patch_size = np.array([args.voxel_size[i] * args.patch_size for i in range(3)]).astype(np.float32)
+        window_size = args.patch_size * args.window_size
+        window_size_sphere = np.array(args.window_size_sphere)
+        model = Model(input_c=args.input_c, 
+            m=args.m,
+            classes=args.classes, 
+            block_reps=args.block_reps, 
+            block_residual=args.block_residual, 
+            layers=args.layers, 
+            window_size=window_size, 
+            window_size_sphere=window_size_sphere, 
+            quant_size=window_size / args.quant_size_scale, 
+            quant_size_sphere=window_size_sphere / args.quant_size_scale, 
+            rel_query=args.rel_query, 
+            rel_key=args.rel_key, 
+            rel_value=args.rel_value, 
+            drop_path_rate=args.drop_path_rate, 
+            window_size_scale=args.window_size_scale, 
+            grad_checkpoint_layers=args.grad_checkpoint_layers, 
+            sphere_layers=args.sphere_layers,
+            a=args.a,
+        )
+    else:
+        raise Exception('architecture {} not supported yet'.format(args.arch))
+
+    if main_process():
+        global logger
+        logger = get_logger(args.save_path)
+        logger.info(args)
+
+    if args.distributed:
+        torch.cuda.set_device(gpu)
+        args.batch_size = int(args.batch_size / ngpus_per_node)
+        args.batch_size_val = int(args.batch_size_val / ngpus_per_node)
+        args.workers = int((args.workers + ngpus_per_node - 1) / ngpus_per_node)
+        if args.sync_bn:
+            if main_process():
+                logger.info("use SyncBN")
+            model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model).cuda()
+        model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[gpu])
+    else:
+        # model = torch.nn.DataParallel(model.cuda())
+        model = model.cuda()
+
+    if main_process():
+        logger.info("=> creating model ...")
+        logger.info("Classes: {}".format(args.classes))
+        logger.info(model)
+        logger.info('#Model parameters: {}'.format(sum([x.nelement() for x in model.parameters()])))
+        if args.get("max_grad_norm", None):
+            logger.info("args.max_grad_norm = {}".format(args.max_grad_norm))
+    
+    if args.weight:
+        if os.path.isfile(args.weight):
+            if main_process():
+                logger.info("=> loading weight '{}'".format(args.weight))
+            checkpoint = torch.load(args.weight, map_location='cpu')
+            model.load_state_dict(checkpoint['state_dict'], strict=True)
+            
+            if main_process():
+                logger.info("=> loaded weight '{}'".format(args.weight))
+        else:
+            logger.info("=> no weight found at '{}'".format(args.weight))
+    
+    # resave
+    if main_process():
+        if not os.path.exists(args.save_path + "/model/"):
+            os.makedirs(args.save_path + "/model/")
+        filename = args.save_path + '/model/model_best_correct.pth'
+        logger.info('Saving checkpoint to: ' + filename)
+        if args.distributed:
+            torch.save({'epoch': checkpoint['epoch'], 'state_dict': model.module.state_dict(), 'optimizer': checkpoint['optimizer'],
+                        'scheduler': checkpoint['scheduler'], 'best_iou': checkpoint['best_iou'], 'is_best': checkpoint['is_best']}, filename)
+        else:
+            torch.save({'epoch': checkpoint['epoch'], 'state_dict': model.state_dict(), 'optimizer': checkpoint['optimizer'],
+                        'scheduler': checkpoint['scheduler'], 'best_iou': checkpoint['best_iou'], 'is_best': checkpoint['is_best']}, filename)
+
+
+if __name__ == '__main__':
+    import gc
+    gc.collect()
+    main()
+    
